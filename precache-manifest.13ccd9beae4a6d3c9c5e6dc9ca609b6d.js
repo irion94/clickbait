4f5d7326e98d77823a83579c4373b5ff32c7bf0d
irion94 <tomasz.kozub218@gmail.com>
2020-05-29 21:15:31 +0200

    Updates

---
+++ precache-manifest.13ccd9beae4a6d3c9c5e6dc9ca609b6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f045f6fbfd89e32fc40a929aeacad234",
+    "url": "/clickbait/static/media/classmate.f045f6fb.png"
+  },
+  {
+    "revision": "daa0fc54a81547241788",
+    "url": "/clickbait/static/js/runtime~main.daa0fc54.js"
+  },
+  {
+    "revision": "128a710b165aa26f73db",
+    "url": "/clickbait/static/js/main.128a710b.chunk.js"
+  },
+  {
+    "revision": "45128b2e45ef11d155cc",
+    "url": "/clickbait/static/js/1.45128b2e.chunk.js"
+  },
+  {
+    "revision": "128a710b165aa26f73db",
+    "url": "/clickbait/static/css/main.73cfcf8c.chunk.css"
+  },
+  {
+    "revision": "d309a033b470feaca0317eede2c53163",
+    "url": "/clickbait/index.html"
+  }
+];
